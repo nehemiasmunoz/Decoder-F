f8e756466ad1d5ab59865cdd5ca04f3e67ec0cc0
Nehemias Muñoz <nehemiasmunozm@gmail.com>
2024-07-29 10:14:12 -0400

    Add: constants file

---
+++ lib/src/data/util/constants.dart
@@ -0,0 +1,20 @@
+//User Table
+const String userTableName = "user";
+const String userColumnId = "_id";
+const String userColumnName = "name";
+const String userColumnAge = "age";
+const String userColumnHasDiabetes = "has_diabetes";
+const String userColumnHasHypertension = "has_hypertension";
+const String userColumnDiabetesType = "diabetes_type";
+const String userColumnHypertensionType = "hypertension_type";
+//Ingredient table
+const String ingredientTableName = "Ingredient";
+const String ingredientColumnId = "_id";
+const String ingredientColumnName = "name";
+const String ingredientColumnDescription = "description";
+const String ingredientColumnRecommendedForDiabetics =
+    "recommended_for_diabetics";
+const String ingredientColumnRecommendeForHypertensive =
+    "recommended_for_hypertensive";
+const String ingredientColumnDiabeticsReasons = "diabetics_reasons";
+const String ingredientColumnHypertensiveReasons = "hypertensives_reasons";
