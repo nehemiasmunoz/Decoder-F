8258c16b91c60576341608c4478af9b9a2e82dbe
Nehemias Muñoz <nehemiasmunozm@gmail.com>
2024-07-29 10:37:19 -0400

    Add: user repo

---
+++ lib/src/domain/repositories/user_repository.dart
@@ -0,0 +1,8 @@
+import 'package:decoder/src/models/user/user_model.dart';
+
+abstract interface class UserRepository {
+  Future<List<User>> getAll();
+  Future<User> insertOne(User user);
+  Future<bool> updateOne(User user);
+  Future<bool> deleteOne(int userId);
+}
