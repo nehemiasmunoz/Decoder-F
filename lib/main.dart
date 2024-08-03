21be78e543d65a86c671c87d636a987ac85a2c48
Nehemias Muñoz <nehemiasmunozm@gmail.com>
2024-08-03 16:46:01 -0400

    Add: ingredient provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:decoder/routes/app_routes.dart';
+import 'package:decoder/src/data/provider/ingredient/ingredient_database_provider.dart';
 import 'package:decoder/src/data/provider/user/form/user_form.dart';
 import 'package:decoder/src/data/provider/user/user_database_provider.dart';
 import 'package:flutter/material.dart';
@@ -15,6 +16,8 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
+        ChangeNotifierProvider<IngredientDatabaseProvider>(
+            create: (ctx) => IngredientDatabaseProvider()),
         ChangeNotifierProvider<UserForm>(create: (ctx) => UserForm()),
         ChangeNotifierProvider<UserDatabaseProvider>(
             create: (ctx) => UserDatabaseProvider()),
