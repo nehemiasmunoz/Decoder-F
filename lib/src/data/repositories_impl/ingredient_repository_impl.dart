3c6d28c3490f78b3cf5853b6fc0548e110752084
Nehemias Muñoz <nehemiasmunozm@gmail.com>
2024-08-05 23:51:21 -0400

    Fix: await on insert/delete method, loop for ingredient creation from db register

--- lib/src/data/repositories_impl/ingredient_repository_impl.dart
+++ lib/src/data/repositories_impl/ingredient_repository_impl.dart
@@ -8,7 +8,7 @@ class IngredientRepositoryImpl implements IngredientRepository {
   @override
   Future<int> deleteIngredient(int ingredientId) async {
     final db = await DatabaseService.instance.database;
-    return db.delete(ingredientTableName,
+    return await db.delete(ingredientTableName,
         where: '$ingredientColumnId = ?', whereArgs: [ingredientId]);
   }
 
@@ -17,21 +17,20 @@ class IngredientRepositoryImpl implements IngredientRepository {
     final db = await DatabaseService.instance.database;
     List<Map<String, Object?>> dbIngredients =
         await db.rawQuery('SELECT * FROM $ingredientTableName');
-
     if (dbIngredients.isNotEmpty) {
       List<Ingredient> ingredientList = [];
-      dbIngredients.map((element) {
-        ingredientList.add(Ingredient.fromMap(element));
-      });
+      for (var item in dbIngredients) {
+        ingredientList.add(Ingredient.fromDBMap(item));
+      }
       return ingredientList;
     }
-
     return Future(() => []);
   }
 
   @override
-  Future<int> insertIngredient(Ingredient ingredient) async {
+  Future<Ingredient> insertIngredient(Ingredient ingredient) async {
     final db = await DatabaseService.instance.database;
-    return db.insert(ingredientTableName, ingredient.toMap());
+    ingredient.id = await db.insert(ingredientTableName, ingredient.toMap());
+    return ingredient;
   }
 }
