8332f3fa222773233857dd7611c3b6e8f83cb473
Nehemias Muñoz <nehemiasmunozm@gmail.com>
2024-08-05 23:52:13 -0400

    Add: provider barrel file

---
+++ lib/src/data/provider/providers.dart
@@ -0,0 +1,3 @@
+export 'package:decoder/src/data/provider/gemini/gemini_provider.dart';
+export 'package:decoder/src/data/provider/ingredient/ingredient_database_provider.dart';
+export 'package:decoder/src/data/provider/user/user_database_provider.dart';
