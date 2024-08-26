7ef115be822f0772e3d644472d78deeabdd1c1a2
Nehemias Muñoz <nehemiasmunozm@gmail.com>
2024-08-25 22:37:18 -0400

    Upd: remove unnecesary properties

--- lib/src/domain/models/user/user_model.dart
+++ lib/src/domain/models/user/user_model.dart
@@ -5,43 +5,28 @@ class User {
   int id = 1;
   String name = "";
   int age = 19;
-  bool hasDiabetes = false;
-  bool hasHypertension = false;
   Diabetes diabetesType = Diabetes.none;
   Hypertension hypertensionType = Hypertension.none;
 
-  void toggleHasDiabetes() {
-    hasDiabetes = !hasDiabetes;
-  }
-
-  void toggleHasHypertension() {
-    hasHypertension = !hasHypertension;
-  }
-
   User();
-  User.withParameters(this.name, this.age, this.hasDiabetes,
-      this.hasHypertension, this.diabetesType, this.hypertensionType);
+  User.withParameters(
+      this.name, this.age, this.diabetesType, this.hypertensionType);
 
   Map<String, Object?> toMap() {
     var map = <String, Object?>{
       userColumnName: name,
       userColumnAge: age,
-      userColumnHasDiabetes: hasDiabetes == true ? 1 : 0,
-      userColumnHasHypertension: hasHypertension == true ? 1 : 0,
       userColumnDiabetesType: diabetesType.name,
       userColumnHypertensionType: hypertensionType.name
     };
     map[userColumnId] = id;
-      return map;
+    return map;
   }
 
   User.fromMap(Map<String, Object?> map) {
     id = int.parse(map[userColumnId].toString());
     name = map[userColumnName].toString();
     age = int.parse(map[userColumnAge].toString());
-    hasDiabetes = map[userColumnHasDiabetes].toString() == "1" ? true : false;
-    hasHypertension =
-        map[userColumnHasHypertension].toString() == "1" ? true : false;
     diabetesType =
         Diabetes.getDiabetesTypeByName(map[userColumnDiabetesType].toString());
     hypertensionType = Hypertension.getHypertensionTypeByName(
