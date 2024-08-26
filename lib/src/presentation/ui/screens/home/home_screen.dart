45dbc4ced4cb3c7a014b307ea3835146f89cce9a
Nehemias Muñoz <nehemiasmunozm@gmail.com>
2024-08-25 22:35:04 -0400

    Upd: new design

--- lib/src/presentation/ui/screens/home/home_screen.dart
+++ lib/src/presentation/ui/screens/home/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:decoder/src/data/provider/ingredient/ingredient_database_provider.dart';
 import 'package:decoder/src/data/provider/user/user_database_provider.dart';
 import 'package:decoder/src/presentation/ui/components/components.dart';
+import 'package:decoder/src/presentation/ui/screens/search/search_ingredient_view.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -21,10 +22,25 @@ class HomeScreen extends StatelessWidget {
             : const DrawerMenu(),
       ),
       appBar: AppBar(
-        title: const Text("Home Screen"),
+        title: const Text(
+          "Home Screen",
+          style: TextStyle(fontSize: 25, fontWeight: FontWeight.w700),
+        ),
         centerTitle: true,
+        actions: [IconButton(onPressed: () {}, icon: const Icon(Icons.camera))],
       ),
       body: const HomeScreenBody(),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () {
+          showModalBottomSheet(
+            isScrollControlled: true,
+            context: context,
+            builder: (ctx) => SearchIngredientView(),
+          );
+        },
+        label: const Text("Search"),
+        icon: const Icon(Icons.search),
+      ),
     );
   }
 }
@@ -35,29 +51,39 @@ class HomeScreenBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: const EdgeInsets.symmetric(horizontal: 16),
-      child: Column(
-        mainAxisSize: MainAxisSize.max,
-        children: [
-          const Searchbar(),
-          const Divider(),
-          Expanded(
-            child: Consumer<IngredientDatabaseProvider>(
-              builder: (BuildContext context, value, Widget? child) {
-                if (value.ingredients.isEmpty) {
-                  return const Text("The ingredient database is empty");
-                }
-                return ListView.builder(
-                  itemCount: value.ingredients.length,
-                  itemBuilder: (context, i) => IngredientTile(
-                    ingredient: value.ingredients[i],
-                  ),
-                );
-              },
+      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
+      child: Consumer<IngredientDatabaseProvider>(
+          builder: (BuildContext context, value, Widget? child) {
+        return Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            Text(
+              "${value.ingredients.length} Ingredients",
+              style: const TextStyle(fontWeight: FontWeight.bold),
             ),
-          )
-        ],
-      ),
+            Expanded(
+              child: Consumer<IngredientDatabaseProvider>(
+                builder: (BuildContext context, value, Widget? child) {
+                  if (value.ingredients.isEmpty) {
+                    return const Center(
+                        child: Text("The ingredient database is empty"));
+                  }
+                  return ListView.builder(
+                    itemCount: value.ingredients.length,
+                    itemBuilder: (context, i) => IngredientTile(
+                      ingredient: value.ingredients[i],
+                    ),
+                  );
+                },
+              ),
+            ),
+            const SizedBox(
+              height: 70,
+            )
+          ],
+        );
+      }),
     );
   }
 }
