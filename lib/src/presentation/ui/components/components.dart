8fe4783c287c629a169446ae30de09f003721dfb
Nehemias Muñoz <nehemiasmunozm@gmail.com>
2024-08-26 16:16:01 -0400

    Upd: delete search bar

--- lib/src/presentation/ui/components/components.dart
+++ lib/src/presentation/ui/components/components.dart
@@ -1,3 +1,2 @@
 export 'package:decoder/src/presentation/ui/components/drawer_menu.dart';
 export 'package:decoder/src/presentation/ui/components/ingredient_tile.dart';
-export 'package:decoder/src/presentation/ui/components/search_bar.dart';
