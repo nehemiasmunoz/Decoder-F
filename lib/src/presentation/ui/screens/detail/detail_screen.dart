6f4001dbb318785ea6d5978af1ff83933faf942e
Nehemias Muñoz <nehemiasmunozm@gmail.com>
2024-08-26 23:50:36 -0400

    Upd: hide/show widgets when has content

--- lib/src/presentation/ui/screens/detail/detail_screen.dart
+++ lib/src/presentation/ui/screens/detail/detail_screen.dart
@@ -10,7 +10,7 @@ class DetailScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
-        title: Text(ingredient.name),
+        title: Text(ingredient.name.toUpperCase()),
       ),
       body: DetailScreenBody(ingredient: ingredient),
     );
@@ -22,24 +22,64 @@ class DetailScreenBody extends StatelessWidget {
   final Ingredient ingredient;
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     return Container(
       padding: const EdgeInsets.symmetric(horizontal: 16),
       child: SingleChildScrollView(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
+            Container(
+              decoration: BoxDecoration(
+                  color: Colors.amber[100],
+                  borderRadius: BorderRadius.circular(5),
+                  border: Border.all(width: 1, color: Colors.amberAccent)),
+              padding: const EdgeInsets.all(10),
+              width: size.width * .9,
+              child: const Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Expanded(flex: 1, child: Icon(Icons.info_outline)),
+                  Expanded(
+                    flex: 4,
+                    child: Text(
+                      "La información generada por IA debe ser considerada como una hipótesis a verificar con un profesional de la salud.",
+                      style: TextStyle(),
+                    ),
+                  ),
+                ],
+              ),
+            ),
             const Text("Description:"),
             Text(ingredient.description),
             const Divider(),
             const Text("Recommended for me"),
             Text(ingredient.recommendedForMe.toString()),
             const Divider(),
-            const Text("Diabetics reasons"),
-            Text(ingredient.diabeticsReasons.replaceAll(RegExp(r'[\[\]]'), '')),
-            const Divider(),
-            const Text("Hypertensive reasons"),
-            Text(ingredient.hypertensiveReasons
-                .replaceAll(RegExp(r'[\[\]]'), '')),
+            Visibility(
+                visible: (ingredient.diabeticsReasons != "[]"),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text("Diabetics reasons"),
+                    Text(
+                      ingredient.diabeticsReasons
+                          .replaceAll(RegExp(r'[\[\]]'), ''),
+                    ),
+                    const Divider(),
+                  ],
+                )),
+            Visibility(
+              visible: (ingredient.hypertensiveReasons != "[]"),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text("Hypertensive reasons"),
+                  Text(ingredient.hypertensiveReasons
+                      .replaceAll(RegExp(r'[\[\]]'), '')),
+                ],
+              ),
+            )
           ],
         ),
       ),
