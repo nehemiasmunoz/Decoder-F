81a30857a2942be7fc15e0ff7f1b66dc8c86c412
Nehemias Muñoz <nehemiasmunozm@gmail.com>
2024-08-26 22:43:36 -0400

    Upd: add recommended section

--- lib/src/presentation/ui/components/ingredient_tile.dart
+++ lib/src/presentation/ui/components/ingredient_tile.dart
@@ -36,7 +36,33 @@ class IngredientTile extends StatelessWidget {
             Expanded(
               child: ListTile(
                 title: Text(ingredient.name),
-                trailing: const Icon(Icons.arrow_forward_ios_sharp),
+                trailing: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                          color: (ingredient.recommendedForMe)
+                              ? Colors.green
+                              : Colors.red,
+                          borderRadius: BorderRadius.circular(5)),
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 4, horizontal: 6),
+                      child: Text(
+                        (ingredient.recommendedForMe)
+                            ? "Recommended"
+                            : "Not recommended",
+                        style: const TextStyle(
+                            color: Colors.white, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 12,
+                    ),
+                    const Icon(Icons.arrow_forward_ios_sharp),
+                  ],
+                ),
               ),
             ),
           ],
