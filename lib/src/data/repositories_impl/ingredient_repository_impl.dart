ff62023659a5a804a9e04ab6e97de24394a3afc7
Nehemias Muñoz <nehemiasmunozm@gmail.com>
2024-08-28 15:34:38 -0400

    Upd: remove empty ingredients

--- lib/src/data/repositories_impl/ingredient_repository_impl.dart
+++ lib/src/data/repositories_impl/ingredient_repository_impl.dart
@@ -5,14 +5,14 @@ import 'package:decoder/src/domain/repositories/ingredient_repository.dart';
 import '../services/database_service.dart';
 
 class IngredientRepositoryImpl implements IngredientRepository {
-  Future<Ingredient> getIngredientIfExist(String name) async {
+  Future<Ingredient?> getIngredientIfExist(String name) async {
     final db = await DatabaseService.instance.database;
     final dbIngredient = await db.rawQuery(
         'SELECT * FROM $ingredientTableName WHERE $ingredientColumnName = "$name"');
     if (dbIngredient.isNotEmpty) {
       return Ingredient.fromDBMap(dbIngredient.first);
     }
-    return Ingredient();
+    return null;
   }
 
   @override
