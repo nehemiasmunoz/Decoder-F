f40f0a0ddc065b0ef51b5327d9bc2455c52107f4
Nehemias Muñoz <nehemiasmunozm@gmail.com>
2024-09-02 16:42:08 -0400

    Upd: id db column name

--- lib/src/data/util/constants.dart
+++ lib/src/data/util/constants.dart
@@ -1,13 +1,13 @@
 //User Table
 const String userTableName = "user";
-const String userColumnId = "_id";
+const String userColumnId = "id";
 const String userColumnName = "name";
 const String userColumnAge = "age";
 const String userColumnDiabetesType = "diabetes_type";
 const String userColumnHypertensionType = "hypertension_type";
 //Ingredient table
 const String ingredientTableName = "ingredient";
-const String ingredientColumnId = "_id";
+const String ingredientColumnId = "id";
 const String ingredientColumnName = "name";
 const String ingredientColumnDescription = "description";
 const String ingredientColumnRecommendedForMe = "recommended_for_me";
