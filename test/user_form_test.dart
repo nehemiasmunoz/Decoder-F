61348a577121112ad239a164b2c34944c5c1db91
Nehemias Muñoz <nehemiasmunozm@gmail.com>
2024-09-04 12:26:10 -0400

    Upd: provider relative path reformat code

--- test/user_form_test.dart
+++ test/user_form_test.dart
@@ -1,18 +1,17 @@
-
-import 'package:decoder/src/data/provider/user/form/user_form.dart';
+import 'package:decoder/controllers/provider/providers.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-main(){
- test("the result of the age validation should be null", (){
-  final userForm = UserForm();
-  final result  = userForm.validateAge("5");
-  expect(result, isNull);
- });
+main() {
+  test("the result of the age validation should be null", () {
+    final userForm = UserForm();
+    final result = userForm.validateAge("5");
+    expect(result, isNull);
+  });
 
- test("the result of the age validation should be a error message", (){
-  final userForm = UserForm();
-  const expected = "Please enter a valid age";
-  final result  = userForm.validateAge("131");
-  expect(result, expected);
- });
+  test("the result of the age validation should be a error message", () {
+    final userForm = UserForm();
+    const expected = "Please enter a valid age";
+    final result = userForm.validateAge("131");
+    expect(result, expected);
+  });
 }
