e7c500699954fee1361fd8e7cc7a12393d90dcfe
Nehemias Muñoz <nehemiasmunozm@gmail.com>
2024-09-07 01:13:21 -0400

    Add: new file with the colors for the app

---
+++ lib/util/colors_app.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+Color get kTextLightColor => const Color.fromARGB(255, 9, 12, 17);
+Color get kBackgroundLightColor => const Color.fromARGB(255, 240, 242, 249);
+Color get kPrimaryLightColor => const Color.fromARGB(255, 65, 97, 200);
+Color get kSecondaryLightColor => const Color.fromARGB(255, 136, 158, 231);
+Color get kAccentLightColor => const Color.fromARGB(255, 93, 126, 234);
+
+Color get kTextDarkColor => const Color.fromARGB(255, 238, 241, 246);
+Color get kBackgroundDarkColor => const Color.fromARGB(255, 6, 8, 15);
+Color get kPrimaryDarkColor => const Color.fromARGB(255, 55, 86, 190);
+Color get kSecondaryDarkColor => const Color.fromARGB(255, 24, 46, 119);
+Color get kAccentDarkColor => const Color.fromARGB(255, 21, 54, 162);
